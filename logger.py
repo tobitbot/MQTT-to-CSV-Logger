9a67a3cae72ad276adc4b1c969299330dc518e75
tobitbot <tobias@my-dogcity.de>
2021-03-31 08:42:19 +0200

    Added exit_handler and guide the user thru arguments if none are given.

--- logger.py
+++ logger.py
@@ -1,23 +1,27 @@
 import paho.mqtt.client as mqtt
 import csv
+import sys
 import argparse
+import atexit
 from datetime import datetime
 
 # Define some defaults
 defaults = {
-    "broker": "localhost",
+    "broker-IP": "localhost",
     "port": 1883,
     "keepalive": 60,
-    "topic": "#",
+    "topic": "application/#",
     "logFile": "logFile.csv",
     "username": "",
     "password": ""
 }
 
+initialLog = False
+
 # Handle user arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--port", help="MQTT-Broker Port")
-ap.add_argument("-b", "--broker", help="MQTT-Broker IP-Address")
+ap.add_argument("-b", "--broker-IP", help="MQTT-Broker IP-Address")
 ap.add_argument("-t", "--topic", help="MQTT topic")
 ap.add_argument("-k", "--keepalive", help="Connection keepalive")
 ap.add_argument("-l", "--logFile", help="CSV filename")
@@ -26,22 +30,45 @@ ap.add_argument("-P", "--password", help="password")
 
 args = vars(ap.parse_args())
 
-# Merge user args and defaults
-for arg in args:
-    if args[arg] != None:
-        defaults[arg] = args[arg]
 
+# If no arguments are given, guide the user thru them
+if len(sys.argv) > 1 :
+    # Merge user args and defaults
+    for arg in args:
+        if args[arg] != None:
+            defaults[arg] = args[arg]
+else :
+    print("No arguments given, define them or use default by leaving the options empty")
+    print("Defaults: " + str(defaults))
+
+    # Merge user args and defaults
+    for arg in defaults:
+        value = input(arg + ": ")
+        if len(value) == 0:
+            continue
+        else:
+            defaults[arg] = value
+
+print("Trying to connect with arguments: ")
+print(str(defaults) + "\n")
+
+
+def exit_handler():
+    print("\nLogged MQTT traffic to log file: " + defaults["logFile"])
+    print("Bye bye... \n")
+
+
+atexit.register(exit_handler)
 
 # Define MQTT functions
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
-        print("Successfully connected to broker " + defaults["broker"] + ":" + str(defaults["port"])
+        print("Successfully connected to broker " + defaults["broker-IP"] + ":" + str(defaults["port"])
             + " on topic \"" + defaults["topic"] + "\" with code:" + str(rc))
 
         with open(defaults["logFile"], "a", newline="") as file:
             writer = csv.writer(file, quotechar="|", delimiter=";")
-            writer.writerow(["Connected to: " + defaults["broker"] + ":" + str(defaults["port"]), datetime.now()])
-
+            writer.writerow(["Connected to: " + defaults["broker-IP"] + ":" + str(defaults["port"]), datetime.now()])
     else:
         print("Could not connect to to broker, code: " + str(rc))
     client.subscribe(str(defaults["topic"]))
@@ -58,7 +85,7 @@ def on_message(client, userdate, msg):
 mqttc = mqtt.Client()
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
-mqttc.connect(str(defaults["broker"]), int(defaults["port"]), int(defaults["keepalive"]))
+mqttc.connect(str(defaults["broker-IP"]), int(defaults["port"]), int(defaults["keepalive"]))
 
 if defaults["username"] != None or defaults["password"]!= None:
     mqttc.username_pw_set(str(defaults["username"]), str(defaults["password"]))
